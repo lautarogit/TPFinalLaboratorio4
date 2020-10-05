86dc5856b20c239f9905ffe275ff982773192e2a
guido <widoroveta@gmail.com>
2020-10-05 07:12:06 -0300

    modificacion de nav

--- views/nav.php
+++ views/nav.php
@@ -1,26 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title></title>
+     <link rel="stylesheet" type="text/css" href="views/assets/layouts/styles/style.css">
+          
+
+</head>
+
+<?php 
+$_SESSION['loggedUser']=null;  ?>
+
 <body>
      <nav class="">
-          <span class="">
-               <strong>Movie Pass</strong>
+          <span class="l-n">
+             <img src="views/assets/img/movie pass.png">
           </span>
           <ul class="">
-               <li class="">
+               <li class="l-n">
                     <a class="nav-link" href="<?php //echo  FRONT_ROOT."CellPhone/ShowAddView "?>">Listar Facturas</a>
                </li>
 
-               <li class="">
+               <li class="l-n">
                     <a class="nav-link" href="<?php //echo  FRONT_ROOT."CellPhone/ShowListView "?>">Agregar Facturas</a>
                </li>
 
-               <?php if($_SESSION['loggedUser']){ ?>
-               <li class="">
+               <?php if($_SESSION['loggedUser']) { ?>
+               <li class="l-n">
                     <a href="<?php echo  FRONT_ROOT."Home/Logout "?>">LOGOUT</a>
                </li>
                <?php }else{ ?>
-               <li class="">
+               <li class="l-n">
                     <a href="<?php echo  FRONT_ROOT."Home/Login "?>">LOGIN</a>
                </li>
                <?php } ?>
           </ul>
      </nav>
 </body>
+</html>
