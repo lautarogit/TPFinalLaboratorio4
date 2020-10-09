b42bfaff1ffbd86c1eca8d94df1964d3bbc3707b
guido <widoroveta@gmail.com>
2020-10-09 16:42:44 -0300

    v1.1

--- models/Movie.php
+++ models/Movie.php
@@ -1,56 +1,5 @@
 <?php
-<<<<<<< HEAD
-namespace Models;
 
-class Movie
-{
-    private $adult;
-    private $backdrop_path;
-    private $belongs_to_collection;
-    private $budget;
-    private $genres; //object {id, name}
-    private $homepage ;
-    private $id;
-    private $imdb_id;
-    private $original_language;
-    private $original_title;
-    private $overview;
-    private $popularity;
-    private $poster_path;
-    private $production_companies; //object {id, name, logo_path, origin_country}
-    private $production_countries; //object {iso_3166_1, name}
-    private $release_date;
-    private $revenue;
-    private $runtime;
-    private $spoken_languages; //object {iso_639_1, name}
-    private $status;
-    private $tagline;
-    private $title;
-    private $video;
-    private $vote_average;
-    private $vote_count;
-
-    public function getTitle ()
-    {
-        return $this->title;
-    }
-
-    public function getId ()
-    {
-        return $this->id;
-    }
-
-     public function setTitle ($title)
-    {
-        $this->title = $title;
-    }
-
-    public function setId ($id)
-    {
-        $this->id = $id;
-    }
-}
-=======
     namespace Models;
 
     class Movie
@@ -166,5 +115,5 @@ class Movie
             $this->status = $status;
         }
     }
->>>>>>> b3fd2d9fb23f10ec7d68377539251a77806cdee7
+
 ?>
