2c8f695167b9d82f0a4e8bd3d49f1a3cb9a390eb
lautarogit <lautarojulianljm@gmail.com>
2020-10-23 19:26:49 -0300

    V2.8 Room CRUD added, Movie-Genres view added

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -20,78 +20,24 @@
             require_once(VIEWS_PATH."home.php");
         }
 
-<<<<<<< HEAD
         public function login($userName, $password)
         {
             $userList = $this->userDAO->GetAll();
-=======
-        public function showCinemaDashboard()
-        {
-            require_once(VIEWS_PATH."validate-session.php");
-            $rolId = $_SESSION['loggedUser']->getRolId();
-
-            if($rolId == 1)
-            {
-                require_once(VIEWS_PATH."cinema-dashboard.php");
-            }
-            else
-            {
-                ?>
-                    <h4 class="text-white">No tiene los permisos necesarios para ingresar a esta página</h4>
-                <?php   
-            }
-        }
-
-        public function showClientCinemaDashboard()
-        {
-            require_once(VIEWS_PATH."validate-session.php");
-            $rolId = $_SESSION['loggedUser']->getRolId();
-
-            if($rolId == 0)
-            {
-                require_once(VIEWS_PATH."client-cinema-dashboard.php");
-            }
-            else
-            {
-                ?>
-                    <h4 class="text-white">No tiene los permisos necesarios para ingresar a esta página</h4>
-                <?php   
-            } 
-        }
-       
->>>>>>> 6868c5abf8cedc06d57ed999e69bcecaafab3641
             
-
- 
-            public function login($userName, $password)
+            foreach($userList as $user)
             {
-                $userList = $this->userDAO->GetAll();
-                
-                foreach($userList as $user)
+                if(($user->getUserName() === $userName) && ($user->getPassword() === $password))
                 {
-                    if(($user->getUserName() === $userName) && ($user->getPassword() === $password))
+                    $_SESSION["loggedUser"] = $user;
+                    
+                    if($user->getRolId() == 0)
                     {
-<<<<<<< HEAD
                         $this->cinemaController->showClientCinemaDashboard();
                         break;
-=======
-                        $_SESSION["loggedUser"] = $user;
-                        
-                        if($user->getRolId() == 0)
-                        {
-                            $this->showClientCinemaDashboard();
-                        }
-    
-                        if($user->getRolId() == 1)
-                        {
-                            $this->showCinemaDashboard();
-                            break;
-                        }   
->>>>>>> 6868c5abf8cedc06d57ed999e69bcecaafab3641
                     }
-                    else
+
+                    if($user->getRolId() == 1)
                     {
-<<<<<<< HEAD
                         $this->cinemaController->showCinemaDashboard();
                         break;
                     }   
@@ -99,12 +45,10 @@
                 else
                 {
                     $this->showLoginView();
-=======
-                        $this->showLoginView();
-                    }
->>>>>>> 6868c5abf8cedc06d57ed999e69bcecaafab3641
                 }
-            } 
+            }
+        } 
+            
         public function showLoginView()
         {
             require_once(VIEWS_PATH."login.php");
--- models/MovieXroom.php
+++ models/MovieXroom.php
@@ -1,52 +1,40 @@
 <?php
+    namespace Models;
 
-namespace Models;
-
-class MovieXroom
-{
-    private $id;
-    private $date;
-    private $movie;
-    private $room;
+    class MovieXroom
+    {
+        private $date;
+        private $movie;
+        private $room;
 
 
 
-    public function __construct($date = ' ', $movie = ' ', $room = ' ')
+    public function __construct($date=' ', $movie=' ', $room=' ')
     {
         $this->date = $date;
         $this->movie = $movie;
         $this->room = $room;
     }
-    public function getId()
-    {
-        return $this->id;
-    }
-    public function setId($id){
-        $this->id=$id;
-    }
-    public function getDate()
-    {
-        return $this->date;
-    }
-    public function getMovie()
-    {
-        return $this->movie;
-    }
 
-    public function getRoom()
-    {
-        return  $this->room;
+        public function getDate (){
+            return $this->date;
+        }
+        public function getMovie (){
+            return $this->movie;
+        }
+        
+        public function getRoom () {
+           return  $this->room;
+        }
+        public function setRoom($room){
+                $this->room = $room;
+        }
+        public function setMovie ($movie){
+            $this->movie=$movie;
+        }
+        public function setDate ($date){
+            $this->date=$date;
+        }
+        
+
     }
-    public function setRoom($room)
-    {
-        $this->room = $room;
-    }
-    public function setMovie($movie)
-    {
-        $this->movie = $movie;
-    }
-    public function setDate($date)
-    {
-        $this->date = $date;
-    }
-}
